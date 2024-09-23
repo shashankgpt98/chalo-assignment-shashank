7148104cab0879791bcdf03e456b4cf3867492e4
shashankgpt98 <shashankgpt07@gmail.com>
2024-09-23 11:39:44 +0530

    remove unused code

--- src/component/type.ts
+++ src/component/type.ts
@@ -1,23 +1,14 @@
 export interface Stop {
-    stopId: string;
-    stopName: string;
-    latitude: number;
-    longitude: number;
-  }
-  
-  export interface Route {
-    routeName: string;
-    direction: "UP" | "DOWN";
-    routeId: string;
-    status: "Active" | "Inactive";
-    stops: Stop[];
-  }
+  stopId: string;
+  stopName: string;
+  latitude: number;
+  longitude: number;
+}
 
-  export interface TransitRoute {
-    routeId: string;
-    name: string;
-    direction: 'UP' | 'DOWN';
-    status: 'Active' | 'Inactive';
-    stops: Stop[];
-  }
-  
+export interface TransitRoute {
+  routeId: string;
+  name: string;
+  direction: 'UP' | 'DOWN';
+  status: 'Active' | 'Inactive';
+  stops: Stop[];
+}
