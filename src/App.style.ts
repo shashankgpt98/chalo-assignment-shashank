9394da8dde85f409e186059502a7602dd48f2edc
vikas sharma <32525848+vikassharma1999@users.noreply.github.com>
2024-10-20 18:07:51 +0000

    FileInput styled moved to style file

--- src/App.style.ts
+++ src/App.style.ts
@@ -3,3 +3,11 @@ import { styled } from "styled-components";
 export const BottomMargin = styled.div`
 margin-bottom: 16px;
 `;
+
+export const FileInput = styled.input`
+margin: 20px 0;
+padding: 10px;
+border: 1px solid #ccc;
+border-radius: 5px;
+font-size: 14px;
+`;
